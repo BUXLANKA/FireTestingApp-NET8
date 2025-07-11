4ed6dae5176134f982a3186dac301369af260160
BUXLANKA <matwey.ishekov@gmail.com>
2025-07-11 20:23:29 +0300

    login page added

--- Viewes/LoginView.xaml.cs
+++ Viewes/LoginView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FireTestingApp_net8
+namespace FireTestingApp_net8.Viewes
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для LoginView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : Page
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
-
-            this.MinHeight = 640;
-            this.MinWidth = 960;
         }
     }
 }
