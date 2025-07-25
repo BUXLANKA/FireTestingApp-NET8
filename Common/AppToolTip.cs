061e386e38417a5d4697fb3df773130e6bcf23a4
BUXLANKA <matwey.ishekov@gmail.com>
2025-07-25 14:37:17 +0300

    реализованная логика поиска

--- Common/AppToolTip.cs
+++ Common/AppToolTip.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace FireTestingApp_net8.Common
 {
-    public static class AppToolTips
+    public static class AppToolTip
     {
         public const string SearchForUser = $"Поиск по имени, фамилии или отчеству";
-        public const string SearchForQuestion = $"Поиск по тексту вопроса";
+        public const string SearchForQuestion = $"Поиск по тексту вопроса и его ID";
     }
 }
